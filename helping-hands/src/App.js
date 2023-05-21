12c0c8e11f62669f4d521421775c02b10090d110
MUNTAAS <98139114+Mhassan0630@users.noreply.github.com>
2023-05-21 09:17:52 -0500

    made the components folder/ pages folder and the files

--- helping-hands/src/App.js
+++ helping-hands/src/App.js
@@ -1,21 +1,35 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { ApolloProvider } from '@apollo/client';
 import Login from './components/Login';
 import './App.css';
+import client from './Apollo';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Login from './pages/login.js';
+import Signup from './pages/Signup';
+import Profile from './pages/Profile';
+import Schools from './pages/Schools';
+
 
 
 function App() {
   return (
+    <ApolloProvider client={client}>
     <Router>
-    <div className="App">
-        <Switch>
-          <Route path="/login">
-            <Login />
-          </Route>
-          {/* TODO: Add other routes here */}
-        </Switch>
-      </div>
-    </Router>
+    <div claassName= "App">
+          <Navbar />
+          <Login />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/profile" component={Profile} />
+            <Route exact path="/schools" component={Schools} />
+          </Switch>
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
