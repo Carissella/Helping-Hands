c2086bc2d0b66ac05b6ca7f25fe959e7d5385ee3
MUNTAAS <98139114+Mhassan0630@users.noreply.github.com>
2023-05-21 09:50:54 -0500

    added structure and location f our client siide GraphQL files inside the src (queries.js/mutations.js

---
+++ helping-hands/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to Helping Hands</h1>
+      {/* You can add more content here */}
+    </div>
+  );
+}
+
+export default Home;
--- helping-hands/src/pages/Login.js
+++ helping-hands/src/pages/Login.js
@@ -1,3 +1,49 @@
-
 import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
+
+const LOGIN_USER = gql`
+  mutation LoginUser($email: String!, $password: String!) {
+    loginUser(email: $email, password: $password) {
+      token
+      user {
+        id
+        email
+      }
+    }
+  }
+`;
+
+function Login() {
+  const [formData, setFormData] = useState({ email: '', password: '' });
+  const [loginUser, { error }] = useMutation(LOGIN_USER);
+
+  const handleChange = (e) => {
+    setFormData({...formData, [e.target.name]: e.target.value});
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await loginUser({
+        variables: { ...formData },
+      });
+      // handle successful login here
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="email" name="email" placeholder="Email" value={formData.email} onChange={handleChange} required />
+        <input type="password" name="password" placeholder="Password" value={formData.password} onChange={handleChange} required />
+        <button type="submit">Log In</button>
+      </form>
+      {error && <p>Error logging in. Please try again.</p>}
+    </div>
+  );
+}
+
+export default Login;
