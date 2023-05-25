6375df7002fc3d45aa0076e93694f2640ec00851
hyusuff <107329715+hyusuff@users.noreply.github.com>
2023-05-25 16:12:22 -0500

    login

--- helping-hands/src/pages/Login.js
+++ helping-hands/src/pages/Login.js
@@ -1,51 +1,114 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import loginSignupImage from "../assest/login-animation.gif";
+import { BiShow, BiHide } from "react-icons/bi";
+import { Link } from "react-router-dom";
+import { toast } from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
+  const navigate = useNavigate();
 
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
+  const handleShowPassword = () => {
+    setShowPassword((preve) => !preve);
   };
 
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setData((preve) => {
+      return {
+        ...preve,
+        [name]: value,
+      };
+    });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    // Perform login logic here, e.g., send data to the server
+    const { email, password } = data;
+    if (email && password) {
+      const fetchData = await fetch(
+        `${process.env.REACT_APP_SERVER_DOMIN}/login`,
+        {
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify(data),
+        }
+      );
 
-    // Display success message or perform any other desired action
-    console.log('Login successful!');
+      const dataRes = await fetchData.json();
+      console.log(dataRes);
+      if (dataRes.alert) {
+        localStorage.setItem(
+          "user",
+          JSON.stringify({ token: dataRes.token, user: dataRes?.data })
+        );
+        toast(dataRes.message);
+        setTimeout(() => {
+          navigate("/");
+          window.location.reload();
+        }, 1000);
+      } else {
+        toast(dataRes.user);
+      }
+    } else {
+      alert("Please Enter required fields");
+    }
   };
 
   return (
-    <div>
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="email">Email:</label>
+    <div className="p-3 md:p-4">
+      <div className="w-full max-w-sm bg-white m-auto flex  flex-col p-4">
+        {/* <h1 className='text-center text-2xl font-bold'>Sign up</h1> */}
+        <div className="w-20 overflow-hidden rounded-full drop-shadow-md shadow-md m-auto">
+          <img src={loginSignupImage} className="w-full" />
+        </div>
+
+        <form className="w-full py-3 flex flex-col" onSubmit={handleSubmit}>
+          <label htmlFor="email">Email</label>
           <input
-            type="email"
+            type={"email"}
             id="email"
-            value={email}
-            onChange={handleEmailChange}
-            required
+            name="email"
+            className="mt-1 mb-2 w-full bg-slate-200 px-2 py-1 rounded focus-within:outline-blue-300"
+            value={data.email}
+            onChange={handleOnChange}
           />
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={handlePasswordChange}
-            required
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
+
+          <label htmlFor="password">Password</label>
+          <div className="flex px-2 py-1 bg-slate-200 rounded mt-1 mb-2 focus-within:outline focus-within:outline-blue-300">
+            <input
+              type={showPassword ? "text" : "password"}
+              id="password"
+              name="password"
+              className=" w-full bg-slate-200 border-none outline-none "
+              value={data.password}
+              onChange={handleOnChange}
+            />
+            <span
+              className="flex text-xl cursor-pointer"
+              onClick={handleShowPassword}>
+              {showPassword ? <BiShow /> : <BiHide />}
+            </span>
+          </div>
+
+          <button className="w-full max-w-[150px] m-auto  bg-red-500 hover:bg-red-600 cursor-pointer  text-white text-xl font-medium text-center py-1 rounded-full mt-4">
+            Login
+          </button>
+        </form>
+        <p className="text-left text-sm mt-2">
+          Don't have account ?{" "}
+          <Link to={"/signup"} className="text-red-500 underline">
+            Sign Up
+          </Link>
+        </p>
+      </div>
     </div>
   );
 };
