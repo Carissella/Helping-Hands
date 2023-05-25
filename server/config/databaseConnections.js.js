733233366f71deb0d159f6c6a46e5569353da3fd
MUNTAAS <98139114+Mhassan0630@users.noreply.github.com>
2023-05-25 15:22:05 -0500

    deleted the old models. school/donation due to change of project route

---
+++ server/config/databaseConnections.js.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const databaseConnection = async () => {
+  try {
+    mongoose.set("strictQuery", false);
+    //mongodb connection
+    const success = await mongoose.connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true, // for warning
+      useUnifiedTopology: true, // for warning
+    });
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.log("Database connection failed");
+  }
+};
+
+module.exports = databaseConnection;
